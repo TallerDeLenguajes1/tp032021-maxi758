4092001cc41df62083ab93b7d07f291e3b3dc709
maxi758 <maxi758@gmail.com>
2021-09-01 18:46:02 -0300

    subiending

---
+++ tp032021/Cadete.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tpnro3_maxi758
+{
+    class Cadete
+    {
+        static int identificador = 0;
+
+        
+
+        public int Id { get; set; };       
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public List<Pedido> ListadoPedidos { get; set; }
+        public Cadete()
+        {
+
+        }
+
+        public Cadete(int id, string nombre, string direccion, string telefono, List<Pedido> listadoPedidos)
+        {
+            Id = id;
+            Nombre = nombre;
+            Direccion = direccion;
+            Telefono = telefono;
+            ListadoPedidos = listadoPedidos;
+        }
+    }
+}
---
+++ tp032021/Cadeteria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tpnro3_maxi758
+{
+    class Cadeteria
+    {
+        public List<Cadete> ListadoDeCadetes { get; set; };
+        public string Nombre { get; set; }
+        public Cadeteria()
+        {
+
+        }
+
+        public Cadeteria(List<Cadete> listadoDeCadetes, string nombre)
+        {
+            ListadoDeCadetes = listadoDeCadetes;
+            Nombre = nombre;
+        }
+    }
+}
